b060566a5c7eb2ef697abc1a2ebee59d5696d22f
rochaalex11021 <rochaalex11021>
2022-09-02 12:40:49 -0500

    ...

--- src/variable.js
+++ src/variable.js
@@ -1,6 +1,6 @@
 /*var greeter = "hi" var-old way and has issues 
-let greeter = "sup" can be changed later on
-const greeter = "yooo" cannot be changed */
+let greeter = "sup" can be changed later on/for variables or values that change ? age?
+const greeter = "yooo" cannot be changed 
 
 const rager = "fun";
 
@@ -10,3 +10,29 @@ console.log(rager)
 console.log(rager)
 console.log(rager)
 console.log(rager)
+
+let greeting = "say hi";
+greeting = "say Hello instead"
+
+console.log(greeting);
+
+
+ let greeting = "say Hi";
+    if (true) {
+        let greeting = "say Hello instead";
+        console.log(greeting); // "say Hello instead"
+    }
+    console.log(greeting); // "say Hi"
+
+    */
+
+    const greeting = {
+        message: "say Hi",
+        times: 4
+    }
+
+
+    greeting.message = "say Hello instead";
+
+
+    console.log(greeting);
